1a2659847d845d549a43aa951fb8a1dfbb038fb9
Ruchitha <Ruchitha3012@gmail.com>
2026-07-14 10:16:08 -0400

    committing changes

--- front-end/script.js
+++ front-end/script.js
@@ -1,1 +1,18 @@
 console.log("Employee Management System Loaded");
+
+document.getElementById("addEmployee").addEventListener("click", function () {
+
+    const name = document.getElementById("name").value;
+
+    const department = document.getElementById("department").value;
+
+    const salary = document.getElementById("salary").value;
+
+    alert(
+        "Employee Details\n\n" +
+        "Name: " + name +
+        "\nDepartment: " + department +
+        "\nSalary: " + salary
+    );
+
+});
